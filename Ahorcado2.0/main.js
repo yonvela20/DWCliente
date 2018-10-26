74ed87a0511074ad1944ebdb8c69a3e086124de3
yonvela20 <yon.vela20@gmail.com>
2018-10-26 22:41:07 +0200

    Ahorcado 2.0

--- Ahorcado2.0/main.js
+++ Ahorcado2.0/main.js
@@ -1,7 +1,26 @@
 window.onload = function(){
     document.getElementById("botonJugar").addEventListener('click', jugar, false);
-    document.getElementById("botonComprobar").addEventListener('click', comprobarLetra, false);
+    document.getElementById("botonComprobar").addEventListener('click', comprobarLetras, false);
+
+    //Hacemos que el input no se vea ya que teoricamente aun no hemos empezado a jugar
+    document.getElementById("inputLetras").style.display = "none";
+    document.getElementById("contenedorLetras").style.display = "none";
+    document.getElementById("botonComprobar").style.display = "none";
 }
+
+let letrasUsadas = [""];
+let palabra = "";
+let arrayPalabras = ["ahorcado","jazz","recta","curva","matematicas","pincho","hola"];
+
+let i = _.random(0, arrayPalabras.length-1);
+p = arrayPalabras[i];
+
+palabra = p.split("");
+
+const ahorcado =  ["A", "H", "O", "R", "C", "A", "D", "O"];
+
+let numAciertos = 0;
+let numErrores = 0;
 
 class Tabla{
     constructor(len){
@@ -16,7 +35,7 @@ class Tabla{
             let filas = document.createElement("tr");
             for(let j = 0; j < this.len; j++){
                 let celda = document.createElement("td");
-                celda.id = j;
+                celda.id = "celdaAcierto"+j;
                 celda.innerHTML = "_ ";
                 filas.appendChild(celda);
             }
@@ -26,15 +45,28 @@ class Tabla{
             contenedor.appendChild(tblBody);
         }
     }
+
+    crearTablaAhorcado(){
+        const contenedor = document.getElementById("contenedorAhorcado");
+        let tabla = document.createElement("table");
+        let tblBody = document.createElement("tbody");
+
+        for(let i = 0; i < 1; i++){
+            let filas = document.createElement("tr");
+            for(let j = 0; j < 8; j++){
+                let celdas = document.createElement("td");
+                celdas.id = "ahorcado"+j;
+                //celdas.innerHTML = "*";
+
+                filas.appendChild(celdas);
+            }
+            tabla.appendChild(filas);
+            tblBody.appendChild(tabla);
+            
+            contenedor.appendChild(tblBody);
+        }
+    }    
 }
-
-let palabra = "";
-let arrayPalabras = ["ahorcado","jazz","recta","curva","matematicas","pincho","hola"];
-
-let i = _.random(0, arrayPalabras.length-1);
-p = arrayPalabras[i];
-
-palabra = p.split("");
 
 class Juego{
     constructor(palabra){
@@ -48,15 +80,90 @@ class Juego{
     get palabra(){
         return this._palabra;
     }
+
+    letraIntroducida(){
+        let letraInput = document.getElementById("introducirLetras").value;
+        let encontrada = false;
+
+        /* if(letraInput.charAt(0) == " " || letraInput == ""){
+            alert("Debes introducir una letra");
+        }
+        */
+        for(let i = 0; i < letrasUsadas.length; i++){
+            if(letraInput.toLowerCase() == letrasUsadas[i].toLowerCase()){
+                encontrada = true;                
+            } 
+        }
+
+        if(encontrada){
+            alert("Ya has introducido esa letra antes");
+        }else{
+            letrasUsadas.push(letraInput);
+            encontrada = false;
+
+            //banderita para el bucle de mas adelante
+            let existe = false;
+
+            for(let j = 0; j < palabra.length; j++){
+                if(letraInput.toLowerCase() == palabra[j].toLowerCase()){
+                    existe = true;
+
+                    numAciertos++;
+
+                    for(let z = 0; z < palabra.length; z++){
+                        //En el caso que el indice de la palabra escogida por el programa coincida con la letra introducida...
+                        if(palabra[z] == letraInput){
+                            //Cogemos todas las rayitas 
+                            let celdaAcierto = document.getElementById("celdaAcierto"+z);
+                            celdaAcierto.innerHTML = palabra[z];
+                        }
+                    }
+                }
+            }
+
+            if(!existe){
+                numErrores++;
+
+                let errores = document.getElementById("pErrores");
+                errores.innerHTML += letraInput+", ";
+                
+                let celdaAhorcado = document.getElementById("ahorcado"+(numErrores-1));
+
+                celdaAhorcado.innerHTML = ahorcado[numErrores-1];
+
+                if(numErrores == ahorcado.length){
+                    document.getElementById("final").innerHTML = "Has perdido :(";
+                }
+            }
+
+            if(numAciertos == palabra.length){
+                document.getElementById("final").innerHTML = "HAS GANADO!";
+            }
+        }
+    }
 }
 
 function jugar(){
+    //Ocultamos el boton de jugar y las regalas y mostramos todo lo demas para continuar jugando
+    document.getElementById("botonJugar").style.display = "none";
+    document.getElementById("reglasJuego").style.display = "none";
+    
+    document.getElementById("inputLetras").style.display = "block";
+    document.getElementById("contenedorLetras").style.display = "block";
+    document.getElementById("botonComprobar").style.display = "block";
+
     juego = new Juego(palabra);
     tabla = new Tabla(palabra.length);
     
     tabla.crearTabla();
+    tabla.crearTablaAhorcado();
+
+    console.log(palabra);
 }
 
-function comprobarLetra(){
-    
+function comprobarLetras(){
+    juego.letraIntroducida();
+    let input = document.getElementById("introducirLetras");
+    input.value = "";
+    input.focus();
 }
