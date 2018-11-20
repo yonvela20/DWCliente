da3a6c3cdf41beccb84bba2984996ab9231c1c44
yonvela20 <yon.vela20@gmail.com>
2018-11-20 13:57:56 +0100

    Haciendo el borrado

--- Carrito/scripts/carro.js
+++ Carrito/scripts/carro.js
@@ -1,68 +1,58 @@
  $(function(){
     $("#citem").val(0); //Ponemos el contador de items a cero cada vez que se refresque la pagina
     $("#cprice").val(0 + " €"); //Ponemos el valor del precio total a cero cada ves que se refresque la pagina 
-    $(".stock").find("input").val(10); //Ponemos el valor del stock a 10 para que todo sea igual al refrescar   
-
-    //var $stockLbl = $(this).find("input"); //Pillamos el input donde esta el stock
-    //var $stock = parseInt($stockLbl.val(), 10); //Lo pasamos a int ya que es un objeto así de primeras
-    var $stockLbl = $(this).find('input'); 
-    var $stock = parseInt($stockLbl.val(), 10);
-
-    $(".item").each(function() {        
-        if($stock > 0) {
-           $(this).on("dblclick", dobleClickItems);
-        }
-    });
     
     $(".item").on("dblclick", dobleClickItems);
+
 }); 
 
-    
 /**
  * FUNCION DOBLE CLIK EN ITEMS
  */
-
-/**
- * FIXME: Que solo se ejecute si hay stock 
- */
 function dobleClickItems(){
-    //Variable necesarias para clonar
-    const $divCarrito = $("#cart_items"); //Pillamos el contenedor del carrito
-    let $idItem = $(this).attr("id"); //Pillamos la id del item seleccionado (devuelve un string)
-    let $clonedItem = $(this).clone(); //Clonamos el item 
+    var $stockLbl = $(".stock", $(this)).text(); //Pillamos la etiqueta del stock 
+    var $cantStock = parseInt($stockLbl.substring(6)); //Cogemos solo el numero y lo pasamos a int 
 
-    var $stockLbl = $(this).find("input"); //Pillamos el input donde esta el stock
-    var $stock = parseInt($stockLbl.val(), 10); //Lo pasamos a int ya que es un objeto así de primeras
-    console.log("Cantidad en stock precompra: " + $stock); //Pruebas: Te muestra la cantidad en stock
+    //Solo si hay stock...
+    if($cantStock > 0){
+        //Variable necesarias para clonar
+        const $divCarrito = $("#cart_items"); //Pillamos el contenedor del carrito
+        let $idItem = $(this).attr("id"); //Pillamos la id del item seleccionado (devuelve un string)
+        let $clonedItem = $(this).clone(); //Clonamos el item 
 
-    let $precioLabel = $(".price", $(this)).text(); //Pillamos el precio del producto clickado
-    let $precio = parseInt($precioLabel.slice(0, 4)); //Cogemos el numero y lo pasamos a int
+        var $stockLbl = $(".stock", $(this)).text(); //Pillamos la etiqueta del stock 
+        var $cantStock = parseInt($stockLbl.substring(6)); //Cogemos solo el numero y lo pasamos a int 
+        //console.log("Cantidad en stock precompra: " + $cantStock); //Pruebas: Te muestra la cantidad en stock
 
-    let $numItems = $("#citem").val(); //Pillamos el valor del input de la cantidad de elementos 
-    parseInt($numItems); //Pasamos el valor a int para poder hacer operaciones
+        let $precioLabel = $(".price", $(this)).text(); //Pillamos el precio del producto clickado
+        let $precio = parseInt($precioLabel.substring(0, 4)); //Cogemos el numero y lo pasamos a int
 
-    //Pillamos el valor del input del precio total y lo pasamos a int tambien 
-    let $precioTotal = parseInt($("#cprice").val().slice(0, 4));
+        let $numItems = $("#citem").val(); //Pillamos el valor del input de la cantidad de elementos 
+        parseInt($numItems); //Pasamos el valor a int para poder hacer operaciones
 
-    compra($stockLbl, $stock, $numItems, $precioTotal, $precio);
-    cloneItem($idItem, $clonedItem, $divCarrito);
-    
-    if($stock == 0 || $stock < 0){
-        $(".stock", $(this)).addClass("agotado");
+        //Pillamos el valor del input del precio total y lo pasamos a int tambien 
+        let $precioTotal = parseInt($("#cprice").val().substring(0, 4));
+
+        compra($(this), $cantStock, $numItems, $precioTotal, $precio);
+        cloneItem($idItem, $clonedItem, $divCarrito);
     }
 }
 
-function compra($stockLbl, $stock, $numItems, $precioTotal, $precio){
-    $stock--; //Contador para decrementar el valor del stock  
-    $stockLbl.val($stock); //Reducimos en uno el numero de stock
+function compra($this, $cantStock, $numItems, $precioTotal, $precio){
+    $cantStock--; //Contador para decrementar el valor del stock  
+    $this.children('.stock').html("Stock " + $cantStock);
 
-    console.log("Cantidad en stock postcompra: " + $stock); //Pruebas: Te muestra la cantidad en stock
+    //console.log("Cantidad en stock postcompra: " + $cantStock); //Pruebas: Te muestra la cantidad en stock
 
     $numItems++; //Incrementamos el numero de items comprados
     $("#citem").val($numItems); //Aumentamos el numero de compras
 
     $precioTotal += $precio; //Al precio del input le vamos sumando el precio de todas la compras 
     $("#cprice").val($precioTotal + " €");
+
+    if($cantStock == 0){
+        $(".stock", $this).addClass("agotado");
+    }
 }
 
 function cloneItem($idItem, $clonedItem, $divCarrito){
@@ -79,7 +69,26 @@ function cloneItem($idItem, $clonedItem, $divCarrito){
     $clonedItem.prepend($delete); //insertamos el enlace al item clonado 
     $divCarrito.prepend($clonedItem); //insertamos el item clonado al div del carrito
 
+    $delete.on("click", borrado);
+
     //Id del item clonado 
     console.log("ID del producto clonado: " + $clonedItem.attr("id"))  //Pruebas
 }
  
+function borrado(){
+    let idPadre = $(this).parent().attr("id");
+    let idItem = idPadre.substring(1);
+
+    //Stock actual
+    let lblStock = $("#" + idItem + " > .stock").html();    
+    let stock = parseInt(lblStock.substring(6));
+
+    stock++;
+    lblStock = stock;
+    $("#" + id + " > .stock").html("Stock "+lblStock);  
+    
+    
+
+    console.log(stock);
+    return false;
+}
