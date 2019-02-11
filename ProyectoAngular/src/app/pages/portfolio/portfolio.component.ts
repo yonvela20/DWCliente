d35029db1c501f99f7e2ec1a84fee1f350b1457c
yonvela20 <yon.vela20@gmail.com>
2019-02-11 09:52:14 +0100

    mas del proyecto angular

--- ProyectoAngular/src/app/pages/portfolio/portfolio.component.ts
+++ ProyectoAngular/src/app/pages/portfolio/portfolio.component.ts
@@ -5,8 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent implements OnInit {
 
+
+export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
